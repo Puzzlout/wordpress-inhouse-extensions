1bb89314c39f15104dbfa1472077c81a007206d3
Jérémie Litzler <contact@puzzlout.com>
2017-11-05 16:42:08 +0100

    Update puzzlout-wp-extensions.php

--- puzzlout-wp-extensions.php
+++ puzzlout-wp-extensions.php
@@ -1,1 +1,9 @@
-
+<?php
+/*
+Plugin Name: Site Plugin for any website
+Description: Site specific code changes for website
+*/
+/* Start Adding Functions Below this Line */
+ 
+ 
+/* Stop Adding Functions Below this Line */
